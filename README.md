e7c273bdf12817426644eab88c4d05b8ea742823
S. Espinet <s.lou.espinet@gmail.com>
2022-01-20 11:11:26 -0800

    add test to check that input is a number
    Co-authored-by: Josh Tillinghast <jwtill@icloud.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+Describe cardMath(number)
+
+Test: "It should check to see if the input consists entirely of numbers."
+Code: cardMath(number);
+boolean
+Expected Output: false
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function cardMath(number) {
+  console.log("Function");
+  if (isNaN(number)) {
+    console.log("branch");
+    return false;
+  console.log("boolean");
+  }
+  else {
+    return true;
+  }
+}
